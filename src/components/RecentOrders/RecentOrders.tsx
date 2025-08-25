60af3d6364f17d86dc3d51cd50f38d071d281cda
MI-Israr <m.israr1228@gamil.com>
2025-08-25 18:11:27 +0500

    dashbord page

--- src/components/RecentOrders/RecentOrders.tsx
+++ src/components/RecentOrders/RecentOrders.tsx
@@ -1,31 +1,43 @@
 import { recentOrders } from "@/Data/recentOrder";
-
+import "./RecentOrder.css";
 const RecentOrders = () => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Product</th>
-          <th>Order ID</th>
-          <th>Date</th>
-          <th>Customer Name</th>
-          <th>Status</th>
-          <th>Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        {recentOrders.map((order, index) => (
-          <tr key={index}>
-            <td>{order.product}</td>
-            <td>{order.orderId}</td>
-            <td>{order.date}</td>
-            <td>{order.customerName}</td>
-            <td>{order.status}</td>
-            <td>{order.amount}</td>
+    <div className="w-[100%] !p-[20px] bg-white rounded-2xl">
+      <table className="w-[100%]">
+        <thead className="border-b !mx-[20px]">
+          <tr className="">
+            <th className="text-start">
+              <input type="checkbox" name="" id="" />
+            </th>
+            <th className="text-start">Product</th>
+            <th>Order ID</th>
+            <th>Date</th>
+            <th>Customer Name</th>
+            <th>Status</th>
+            <th>Amount</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {recentOrders.map((order, index) => (
+            <tr
+              style={{ padding: "30px" }}
+              key={index}
+              className="text-center text-[14px]"
+            >
+              <td className="text-start">
+                <input type="checkbox" name="" id="" />
+              </td>
+              <td className="text-start">{order.product}</td>
+              <td>#{order.orderId}</td>
+              <td>{order.date}</td>
+              <td>{order.customerName}</td>
+              <td>{order.status}</td>
+              <td>{order.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
