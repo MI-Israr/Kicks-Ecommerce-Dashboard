68947f6e9a5837a04dc4884bb8da126db8bc0c25
MI-Israr <m.israr1228@gamil.com>
2025-08-26 15:56:29 +0500

    Orders List Table issues fixed

--- src/components/RecentOrders/RecentOrders.tsx
+++ src/components/RecentOrders/RecentOrders.tsx
@@ -3,9 +3,17 @@ import "./RecentOrder.css";
 const RecentOrders = () => {
   return (
     <div className="w-[100%] !p-[20px] bg-white rounded-2xl">
+      <div className="w-full border-b border-gray-300 !py-[12px] flex justify-between">
+        <h1 className="font-bold text-[24px]">Recent Orders</h1>
+        <img
+          src="/src/assets/three-dots-vertical-bold.svg"
+          alt=""
+          className="cursor-pointer"
+        />
+      </div>
       <table className="w-[100%]">
-        <thead className="border-b !mx-[20px]">
-          <tr className="">
+        <thead className="border-b border-gray-300 !mx-[20px]">
+          <tr>
             <th className="text-start">
               <input type="checkbox" name="" id="" />
             </th>
@@ -22,7 +30,7 @@ const RecentOrders = () => {
             <tr
               style={{ padding: "30px" }}
               key={index}
-              className="text-center text-[14px]"
+              className="text-center text-[14px] border-b border-gray-300"
             >
               <td className="text-start">
                 <input type="checkbox" name="" id="" />
@@ -31,8 +39,17 @@ const RecentOrders = () => {
               <td>#{order.orderId}</td>
               <td>{order.date}</td>
               <td>{order.customerName}</td>
-              <td>{order.status}</td>
-              <td>{order.amount}</td>
+              <td className="flex items-center justify-center gap-2">
+                <span
+                  className={`w-[10px] h-[10px] rounded-4xl ${
+                    order.status === "Delivered"
+                      ? " bg-[#4A69E2]"
+                      : "bg-[#FFA52F]"
+                  }`}
+                ></span>
+                {order.status}
+              </td>
+              <td>${order.amount}</td>
             </tr>
           ))}
         </tbody>
--- src/pages/Orders List/OrdersList.tsx
+++ src/pages/Orders List/OrdersList.tsx
@@ -1,5 +1,13 @@
+import PageTitle from "@/components/PageTitle/PageTitle";
+import RecentOrders from "@/components/RecentOrders/RecentOrders";
+
 const OrdersList = () => {
-  return <div>OderList</div>;
+  return (
+    <div className="flex flex-col gap-[24px]">
+      <PageTitle title="Orders List" rightSideType="date" />
+      <RecentOrders />
+    </div>
+  );
 };
 
 export default OrdersList;
